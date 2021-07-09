b75a7946e29eaf20cd5e8ccc14af3d1462185ed4
tkmanabat <tkmanabat@gmail.com>
2021-07-09 22:55:13 +0800

    add fixes to UI

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ if 'n' not in st.session_state:
 
 
 def show_transition(sample_string,n):
-    st.write('### Selected Regular Expression: ```' + regex+'```')
     st.write('### String Checking: `'+ sample_string[0:n]+'`')
-    #st.graphviz_chart(dfa.show_diagram(sample_string[0:n]))
+
+
     graph=dfa.show_diagram(sample_string[0:n])
     
     graph.format="svg"
@@ -47,37 +47,32 @@ st.write ('Web application for step-by-step visual simulation of deterministic f
 
 st.sidebar.write('# Input Panel 📚')
 st.sidebar.write(' Please select problem and input sample string for testing.')
-selection=st.sidebar.selectbox("Select Regular Expression",['Problem 1 (a,b)','Problem 2 (1,0)'])
+selection=st.sidebar.selectbox("Select Regular Expression",['(a+b)(a+b)*(aa+bb)(ab+ba)(a+b)*(aba+baa)','(11+00)(1+0)*(101+111+01)(00*+11*)(1+0+11)'])
 
 problem1='(a+b)(a+b)*(aa+bb)(ab+ba)(a+b)*(aba+baa)'
 problem2='(11+00)(1+0)*(101+111+01)(00*+11*)(1+0+11)'
 
+with st.sidebar:
+        if selection=='(a+b)(a+b)*(aa+bb)(ab+ba)(a+b)*(aba+baa)':
+            dfa=dfa
+            regex=problem1
+
+        elif selection=='(11+00)(1+0)*(101+111+01)(00*+11*)(1+0+11)':
+            dfa=dfa2
+            regex=problem2
+
+st.write('### Selected Regular Expression: ```' + regex+'```')
 
 with st.form(key='input'):
     with st.sidebar:
-        if selection=='Problem 1 (a,b)':
-            dfa=dfa
-            regex=problem1
-            
-
-        elif selection=='Problem 2 (1,0)':
-            dfa=dfa2
-            regex=problem2
-            
-
         sample_string=st.text_input('Enter Sample String:')
-       
-
-        
         submit_button=st.form_submit_button(label='Test')
 
         simulate_bool=st.checkbox('Simulate steps')
 
-        
-
 
 if submit_button:
-    st.write('### Selected Regular Expression: ```' + regex+'```')
+    #st.write('### Selected Regular Expression: ```' + regex+'```')
 
     if sample_string[0]== 'a' or 'A' or 'b' or 'B':
             sample_string=sample_string.lower()
@@ -85,11 +80,10 @@ if submit_button:
     sample_string_bool=dfa.input_check(sample_string)
 
 
-    
 
     graph=dfa.show_diagram(sample_string)
     graph.format="svg"
-    
+
     graph.render("test")
     f = open("test.svg","r")
     lines = f.readlines()
@@ -104,15 +98,36 @@ if submit_button:
 
     st.write("### Tabulated Steps: ")
     col1, col2, col3 = st.beta_columns(3)
-    with col2:
+    with col1:
         st.dataframe(dfa.table,width=1100,height=500)
+
+    if selection=="(a+b)(a+b)*(aa+bb)(ab+ba)(a+b)*(aba+baa)":
+        with col2:
+            st.write("### Context Free Grammar")
+            st.write("S → ABCDBE")
+            st.write("A → a | b")
+            st.write("B → aB | bB | ^")
+            st.write("C → aa | bb")
+            st.write("D → ab | ba")
+            st.write("E → aba | baa")
+    elif selection=="(11+00)(1+0)*(101+111+01)(00*+11*)(1+0+11)":
+        with col2:
+            st.write("### Context Free Grammar")
+            st.write("S → ABCDG")
+            st.write("A → 11 | 00")
+            st.write("B → 1B | 0B | ^")
+            st.write("C → 101 | 111 | 01")
+            st.write("D → E | F")
+            st.write("E → 00E | ^")
+            st.write("F → 11F | ^")
+            st.write("G → 1 | 0 | 11")
 
 if simulate_bool:
     st.sidebar.write('# Simulator Navigation')
     st.sidebar.write(' Use the buttons below to simulate the sample string thru the DFA.')
     col1, col2 = st.sidebar.beta_columns([1.8,1])
     with col2:
-        next_button=st.button('Next >') 
+        next_button=st.button('Next >')
     with col1:
         back_button=st.button('< Back')
 
